2e51d92d07e5d1129286e65cc822d22af7d70603
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-02 16:47:16 -0700

    starting seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+u1 = User.create!(name: "Michaela Brown", title: "Founder", email: "michaela.brown375@gmail.com", admin: true, password: "owner")
+
+c1 = Category.create!(name: "Essentials", description: "Pieces needed to make jewelry.")
