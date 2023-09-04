aa746ec1a2f6fffa0b0c9831868f2472699b5081
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-03 19:32:18 -0700

    users controller index, show, and create methods are built out in the userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
     skip_before_action :authenticate_user
 
+    def index 
+        render json: User.all, status: :ok
+    end
+
+    def show
+        render json: current_user, status: :ok
+    end
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :ok
+    end
+
+    private 
+
+    def user_params
+        params.permit(:name, :title, :email, :password, :admin)
+    end 
 end
