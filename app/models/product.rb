f8fbdff545ce0a69ce253f04e6481cc76d642663
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-03 19:25:27 -0700

    product status must be low, full, or mid

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :quantity, presence: true
-  validates :status, presence: true
+  validates :status, presence: true, inclusion: { in: %w(full low mid) }
   validates :price, presence: true
 
   belongs_to :category
