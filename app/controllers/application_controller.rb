63709b5cf30a9bdad21848b289524d24cc9a7336
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-03 19:28:58 -0700

    defined current_user in applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+      end
+
     private
 
     def render_unprocessable_entity_response(exception)
