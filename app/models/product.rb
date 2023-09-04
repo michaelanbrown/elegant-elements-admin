f50fb82c6a583221ea6fc1589c66756805cbb4a8
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-03 19:23:31 -0700

    product must have name, quantity, status, and price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
+
+  validates :name, presence: true
+  validates :quantity, presence: true
+  validates :status, presence: true
+  validates :price, presence: true
+
   belongs_to :category
 end
