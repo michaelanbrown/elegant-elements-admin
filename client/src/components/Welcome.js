33f8735008a606956976e7a0fb8ae813080c0cd1
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-05 17:59:30 -0700

    welcome page styling fixed

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -8,11 +8,11 @@ function Welcome() {
         <div>
             <br />
             <div>
-                <img src={"https://cdn.shopify.com/s/files/1/0008/8932/3571/products/DSC_0019_800x800_1200x1200.jpg?v=1541563917"} alt="Handstamped Necklace Photo" width="80%" height="80%"/>
+                <h1>Elegant Elements</h1>
+                <h2>Admin</h2>
+                <img src={"https://cdn.shopify.com/s/files/1/0008/8932/3571/products/DSC_0019_800x800_1200x1200.jpg?v=1541563917"} alt="Handstamped Necklace Photo" width="30%" height="30%"/>
                 <br/>
                 <p className='welcome'>Here at Elegant Elements we are blessed to be able to provide our customers with the ability to be part of the creation of their own piece.</p>
-                <br/>
-                <div className='welcomebig'>Background</div>
                 <p className='welcome'>Our inspiration comes from promoting good mental health.
                 We started out creating jewerly with inspirational messages so that in a time of need one can look down and remember positive affirmations.
                 As time went on, we decided to expand to any personalized message that a customer desires.
