959daf4b23a5b79931a8fd100df8095f1be881f2
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-06 06:35:47 -0700

    added product_params as a private method in product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,10 @@ class ProductsController < ApplicationController
     def show
         render json: @product, status: :ok
     end
+
+    private
+
+    def product_params
+        params.permit(:category_id, :name, :status, :quantity, :price)
+    end
 end
