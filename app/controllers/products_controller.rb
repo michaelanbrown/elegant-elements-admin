a6d6a2f801cda1b096256ad061160b5aae029524
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-07 16:58:51 -0700

    before_action in productcontroller is also needed for update method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-    before_action :find_product, only: [:show, :destroy]
+    before_action :find_product, only: [:show, :update, :destroy]
     before_action :authorize_user_is_admin, only: [:destroy]
 
     def index 
