dd8652e0f8323ede6b120065768d15e6c72c0045
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-07 17:07:56 -0700

    added header.js with header content

---
+++ client/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import NavBar from "./NavBar";
+
+function Header({ productCount, setProductCount, custAddresses }) {
+
+  return (
+    <div className="Header">
+      <header>
+        <div>
+            <h1 className="elegant-header">Elegant Elements</h1>
+            <p className="active"><em>✨Adding a touch of inspiration and elegance to your every day life✨</em></p>
+            <img className="eleganceimg" src={"https://i.imgur.com/upX3ZLK.png"} alt="Elegant Elements" width="20%" height="20%"/>
+        </div>  
+      </header>
+    </div>
+  );
+}
+
+export default Header;
