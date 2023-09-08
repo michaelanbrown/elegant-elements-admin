f1fd4635efd9491ccfbd3e3ddbf943f8cbda603b
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-07 17:04:05 -0700

    added post route to routes.rb for sessions create method

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :products
   resources :categories
   resources :users, only: [:index, :show, :create]
+
+  post "/login", to: "sessions#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
