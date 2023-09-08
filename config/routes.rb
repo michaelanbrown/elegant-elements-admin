97d41d3d3b594f0f6f72fbf506cb7f70a6b665b7
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-07 17:04:47 -0700

    added a route to routes.rb for sessions destroy

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :create]
 
   post "/login", to: "session#create"
+  delete "/logout", to: "session#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
