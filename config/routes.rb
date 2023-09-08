134ae6e397127e5d98a479e2ee8499d658bfbd5d
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-07 17:04:25 -0700

    typo corrected in route. it's session instead of sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :users, only: [:index, :show, :create]
 
-  post "/login", to: "sessions#create"
+  post "/login", to: "session#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
