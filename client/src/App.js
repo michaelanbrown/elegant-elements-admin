c598dcc3a182728f25a900cbe3f60303530a551c
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-07 17:08:33 -0700

    added header to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Switch, Route,  } from "react-router-dom";
 import Welcome from "./components/Welcome";
+import Header from "./components/Header";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <BrowserRouter>
         <Switch>
           <Route path="/"><Welcome/></Route>
