c5180ed383d7cafffd0129867ee6d6b2b964dbb0
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-07 17:02:55 -0700

    added create method to sessionscontroller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,14 @@
 class SessionController < ApplicationController
     skip_before_action :authenticate_user
 
+    def create
+        user = User.find_by_email(params[:email])
+        if user&.authenticate(params[:password])
+          session[:user_id] = user.id
+          render json: user, status: :ok
+        else 
+          render json: { errors: "Invalid Credentials" }, status: :unauthorized
+        end
+    
+      end
 end
