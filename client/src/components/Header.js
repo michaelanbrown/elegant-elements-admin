c0cdf42c5cc30912940c4b319fa5aa1c43f5c27d
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-11 15:52:21 -0700

    made font size bigger

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,9 +8,9 @@ function Header({ productCount, setProductCount, custAddresses }) {
       <header>
         <div>
             <h1 className="elegant-header">Elegant Elements</h1>
-            <h2>Admin</h2>
+            <h1>Admin</h1>
             <br/>
-            <p className="active"><em>✨Adding a touch of inspiration and elegance to your every day life✨</em></p>
+            <p className="active"><em className="welcome">✨Adding a touch of inspiration and elegance to your every day life✨</em></p>
             <img className="eleganceimg" src={"https://i.imgur.com/upX3ZLK.png"} alt="Elegant Elements" width="20%" height="20%"/>
         </div>  
       </header>
