7cee93c53c1e0a726d775e673efc648f90260334
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-13 18:37:10 -0700

    added a context folder and user context file

---
+++ client/src/components/context/User.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react"
+
+const UserContext = React.createContext();
+
+function UserProvider({ children }) {
+
+  return (
+
+  );
+}
+
+export { UserContext, UserProvider };
