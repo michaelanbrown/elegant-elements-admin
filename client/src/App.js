8dccdd53aeebf51d9dad13e1fd327b04852237bd
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-13 18:41:53 -0700

    added currentuser to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import { BrowserRouter, Switch, Route,  } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import Header from "./components/Header";
 import './App.css';
+import { UserContext } from "./components/context/User";
+import { useContext } from 'react';
 
 function App() {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+
   return (
     <div className="App">
       <Header />
