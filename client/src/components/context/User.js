5667ead87812f1583fe1f0aac95949e1df68b169
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-13 18:37:54 -0700

    built out uder context

--- client/src/components/context/User.js
+++ client/src/components/context/User.js
@@ -3,9 +3,12 @@ import React, { useState } from "react"
 const UserContext = React.createContext();
 
 function UserProvider({ children }) {
+    const [currentCustomer, setCurrentCustomer] = useState(false)
 
   return (
-
+    <UserContext.Provider value={{ currentCustomer, setCurrentCustomer }}>
+      {children}
+    </UserContext.Provider>
   );
 }
 
