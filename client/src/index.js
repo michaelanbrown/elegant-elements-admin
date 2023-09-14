518a3e33fa5bfe90468e8d42a547abd88f65df27
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-13 18:44:49 -0700

    removed import { UserContext } from './components/context/User'; from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { UserContext } from './components/context/User';
 import { UserProvider } from './components/context/User';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
