1dd737faee42029c04b5f19661f49f0642693a46
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-13 18:49:53 -0700

    removed login pfunction from welcome page and to login page

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,38 +1,9 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import '../App.css'
 import { UserContext } from './context/User';
 
 function Welcome() {
     const { currentUser, setCurrentUser } = useContext(UserContext);
-    const [errors, setErrors] = useState([])
-    const [formData, setFormData] = useState({
-        email: "",
-        password: "",
-    })
-    const {email, password} = formData
-
-    function onLogin(e){
-        e.preventDefault()
-        const user = {
-            email,
-            password
-        }
-       
-        fetch("/login",{
-          method:'POST',
-          headers:{'Content-Type': 'application/json'},
-          body:JSON.stringify(user)
-        })
-        .then(res => {
-            if(res.ok){
-                res.json().then(user => {
-                    setCurrentUser(user)
-                })
-            } else {
-                res.json().then(json => setErrors([json.errors]))
-            }
-        }) 
-    }
 
     return (
         <div>
