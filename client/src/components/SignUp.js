fe69028baafd6d35b6e1bcaa877cb8c5cdc57891
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-14 17:49:02 -0700

    imported usercontext and currentuser into signup.js

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,10 +1,10 @@
-import React from 'react'
-
+import React, { useState, useContext } from 'react';
 import '../App.css'
+import { UserContext } from './context/User';
 
 
 function Signup() {
-
+    const { currentUser, setCurrentUser } = useContext(UserContext);
 
     return (
         <> 
