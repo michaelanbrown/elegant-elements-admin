6d2107baf257b3f4cfc4fea2426dc551c51ab421
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-15 17:39:04 -0700

    imported NavLink from react-router-dom into navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import '../App.css'
 import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
 import { UserContext } from './context/User';
 
 export default function NavBar ({})  {
