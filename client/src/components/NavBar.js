19bdca1661a459b8db892e94d66cb009e52b4c5d
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-15 17:37:26 -0700

    added usercontext to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,8 +1,9 @@
 import '../App.css'
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from './context/User';
 
 export default function NavBar ({})  {
-
+    const { currentUser, setCurrentUser } = useContext(UserContext);
 
     return (
         <div>
