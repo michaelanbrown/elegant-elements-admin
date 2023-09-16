959dacbfad66b9414265b34b1893d4a191b84666
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-15 17:54:40 -0700

    imported useNavigate in navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import '../App.css'
 import React, { useContext } from "react";
-import { BrowserRouter, NavLink } from "react-router-dom";
+import { BrowserRouter, NavLink, useNavigate } from "react-router-dom";
 import { UserContext } from './context/User';
 
 export default function NavBar ({})  {
