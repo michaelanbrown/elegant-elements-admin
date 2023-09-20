abf3472479dc19dcf8263753db2cf9cb766598ea
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-20 06:24:30 -0700

    imported css to productmap.js

--- client/src/components/ProductMap.js
+++ client/src/components/ProductMap.js
@@ -1,10 +1,11 @@
 import React from "react";
+import '../App.css'
 
 function ProductMap({ prod, name, quantity, price, status }) {
 
   return (
     <div>
-
+      <h1>{name}</h1>
     </div>
   );
 }
