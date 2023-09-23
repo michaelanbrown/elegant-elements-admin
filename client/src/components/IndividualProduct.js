d8c0cdc4d1cfc9749ce49007ea0cbc7c1f202521
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-23 14:10:21 -0700

    importing useParams into individualproduct,js

--- client/src/components/IndividualProduct.js
+++ client/src/components/IndividualProduct.js
@@ -1,10 +1,11 @@
 import '../App.css'
 import React, { useContext, useEffect } from "react";
 import { UserContext } from './context/User';
+import { useParams } from 'react-router-dom';
 
 function IndividualProduct({ products, setProducts }) {
     const { currentUser, setCurrentUser } = useContext(UserContext);
-
+    console.log()
     useEffect(() => {
         fetch()
 
