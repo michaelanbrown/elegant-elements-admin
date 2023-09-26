96c559368fa9c0c3bce44766277630ae83a292c5
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-26 07:18:29 -0700

    pulling in setCategories as a prop

--- client/src/components/CategoryMap.js
+++ client/src/components/CategoryMap.js
@@ -2,7 +2,7 @@ import '../App.css'
 import React, { useContext } from "react";
 import { UserContext } from './context/User';
 
-function CategoryMap({ categories }) {
+function CategoryMap({ categories, setCategories }) {
     const { currentUser, setCurrentUser } = useContext(UserContext);
 
     return (
