7cd1e7bc2a6e8b422c383af8f090d044eae1f749
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-26 07:19:32 -0700

    importing categorymap into categories

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,6 +1,7 @@
 import '../App.css'
 import React, { useContext } from "react";
 import { UserContext } from './context/User';
+import CategoryMap from './CategoryMap';
 
 function Categories({ categories, setCategories }) {
   const { currentUser, setCurrentUser } = useContext(UserContext);
