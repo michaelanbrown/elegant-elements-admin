3578261cf63eb47d663ee1cb007e0456171d6d64
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-26 07:19:14 -0700

    importing currentUser into categories

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,6 +1,9 @@
-import React from "react";
+import '../App.css'
+import React, { useContext } from "react";
+import { UserContext } from './context/User';
 
 function Categories({ categories, setCategories }) {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
 
   return (
     <div>
