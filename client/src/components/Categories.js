0a455bcd59500eaec20ca0dc5ff0957cb8658a08
Michaela Brown <m_brownbrown@yahoo.com>
2023-09-30 12:20:58 -0700

    imported useState

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,5 +1,5 @@
 import '../App.css'
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from './context/User';
 import CategoryMap from './CategoryMap';
 
