ea1df1d99d1373eafc7baee6a02cc612e73863d5
Michaela Brown <m_brownbrown@yahoo.com>
2023-10-01 10:55:10 -0700

    added productFiltering usestate

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -4,6 +4,7 @@ import ProductMap from "./ProductMap";
 
 function Products({ products, setProducts }) {
   const { currentUser, setCurrentUser } = useContext(UserContext);
+  const [productFiltering, setProductFiltering] = useState("")
 
   const productMap = products.sort((a, b) => a.name.localeCompare(b.name)).map(prod => {
     return <ProductMap key={prod.id} prod={prod} name={prod.name} price={prod.price} quantity={prod.quantity} status={prod.status}/>
