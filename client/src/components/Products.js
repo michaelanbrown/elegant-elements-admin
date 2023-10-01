927e3fae7dafc399429e39aa157947720731d503
Michaela Brown <m_brownbrown@yahoo.com>
2023-10-01 10:54:10 -0700

    adding useContext into products

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ProductMap from "./ProductMap";
 
 function Products({ products, setProducts }) {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
 
   const productMap = products.sort((a, b) => a.name.localeCompare(b.name)).map(prod => {
     return <ProductMap key={prod.id} prod={prod} name={prod.name} price={prod.price} quantity={prod.quantity} status={prod.status}/>
