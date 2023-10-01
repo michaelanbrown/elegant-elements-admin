87a5f8bf30f82ff86fcc96e1707791555e88c18f
Michaela Brown <m_brownbrown@yahoo.com>
2023-10-01 10:55:49 -0700

    addedfilteredProducts const

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -6,6 +6,8 @@ function Products({ products, setProducts }) {
   const { currentUser, setCurrentUser } = useContext(UserContext);
   const [productFiltering, setProductFiltering] = useState("")
 
+  const filteredProducts = products.filter(product => product.name.toLowerCase().indexOf(productFiltering.toLowerCase()) > -1)
+
   const productMap = products.sort((a, b) => a.name.localeCompare(b.name)).map(prod => {
     return <ProductMap key={prod.id} prod={prod} name={prod.name} price={prod.price} quantity={prod.quantity} status={prod.status}/>
   })
