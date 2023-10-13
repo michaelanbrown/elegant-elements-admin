955cf60039492d9e3150c556a95bbb4ab5e788b0
michaelanbrown <m_brownbrown@yahoo.com>
2023-10-13 16:25:02 -0400

    product drop down complete

--- client/src/components/CategoryMap.js
+++ client/src/components/CategoryMap.js
@@ -8,7 +8,9 @@ function CategoryMap({ category, categories, setCategories }) {
     //add a drop down for products in category
     //each drop down item will direct you to the individual products page for that product
 
-    const productDropDown = category.products.map()
+    const productDropDown = category.products.map(product => {
+        <li>{product.name}</li>
+    })
 
     return (
         <div>
