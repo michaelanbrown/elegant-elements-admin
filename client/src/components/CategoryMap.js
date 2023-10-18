7e35251dea143e9271aca0e51d93be3e3ef7b15c
Michaela Brown <m_brownbrown@yahoo.com>
2023-10-18 08:33:45 -0700

    added useState for showWording

--- client/src/components/CategoryMap.js
+++ client/src/components/CategoryMap.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 function CategoryMap({ category, categories, setCategories }) {
     const { currentUser, setCurrentUser } = useContext(UserContext);
     const [show, setShow] = useState(false)
-
+    const [showWording, setShowWording] = useState("Show Products")
 
     const productDropDown = category.products.map(product => 
         <li key={product.id}><Link className='navbarlink' to={`/products/${product.id}`}>{product.name}</Link></li>
