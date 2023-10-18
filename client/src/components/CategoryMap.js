95a18be9100f659f629dd4866f1d5c89b0390e68
Michaela Brown <m_brownbrown@yahoo.com>
2023-10-18 08:35:44 -0700

    page break only shows when products are not shown

--- client/src/components/CategoryMap.js
+++ client/src/components/CategoryMap.js
@@ -24,7 +24,7 @@ function CategoryMap({ category, categories, setCategories }) {
             <p>{category.description}</p>
             <button onClick={showProducts}>{showWording}</button>
             {show ? productDropDown : null}
-            <br/>
+            {show ? null : <br/>}
             <br/>
         </div>
     )
