6ea8a863ce3a64434845def196385c28f8f0ea25
Michaela Brown <m_brownbrown@yahoo.com>
2023-10-31 19:02:40 -0700

    started getUsers function

--- client/src/App.js
+++ client/src/App.js
@@ -56,6 +56,10 @@ function App() {
     })
   }
 
+  function getUsers() {
+    
+  }
+
   return (
     <div className="App">  
       <BrowserRouter>
--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -4,6 +4,8 @@ import React from "react";
 function Users ({ users, setUsers })  {
 
 // admins can change a user's title and admin boolean
+
+console.log(users)
     return (
         <></>
     )
