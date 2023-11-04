79fe07af71bf89c17a822e5697b1b8256d7d3872
Michaela Brown <m_brownbrown@yahoo.com>
2023-11-03 17:53:39 -0700

    added usermap to return statement

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -4,10 +4,12 @@ import React from "react";
 function Users ({ users, setUsers })  {
 
 // admins can change a user's title and admin boolean
-    const userMap = 
+    const userMap = users.map(user => {
+
+    })
 
     return (
-        <></>
+        <>{userMap}</>
     )
 }
 
