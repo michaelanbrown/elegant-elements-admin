3bc794f62406082e492fc714410a66ffb0a44467
Michaela Brown <m_brownbrown@yahoo.com>
2023-11-06 18:22:26 -0800

    added onEdit to onClick for button

--- client/src/components/UserMap.js
+++ client/src/components/UserMap.js
@@ -10,7 +10,7 @@ function onEdit() {
 
     return (
         <div>
-            <h3 className='wayLessSpace'>{user.name}{" "}<button className='userEdit'>Edit ✏️</button></h3>
+            <h3 className='wayLessSpace'>{user.name}{" "}<button onClick={onEdit} className='userEdit'>Edit ✏️</button></h3>
             <p className='wayLessSpace'>{user.title}</p>
             <p className='wayLessSpace'>{user.email}</p>
             {user.admin ? <p className='wayLessSpace'>Admin Status: True</p> : <p className='wayLessSpace'>Admin Status: False</p> }
