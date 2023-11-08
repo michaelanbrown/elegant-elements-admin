1cc23cd9a16e62b076f24ca11cf7f6c4b8c0c784
Michaela Brown <m_brownbrown@yahoo.com>
2023-11-07 16:18:16 -0800

    put UserMap in another div

--- client/src/components/UserMap.js
+++ client/src/components/UserMap.js
@@ -10,12 +10,14 @@ function onEdit() {
 
     return (
         <div>
-            <h3 className='wayLessSpace'>{user.name}{" "}<button onClick={onEdit} className='userEdit'>Edit ✏️</button></h3>
-            <p className='wayLessSpace'>{user.title}</p>
-            <p className='wayLessSpace'>{user.email}</p>
-            {user.admin ? <p className='wayLessSpace'>Admin Status: True</p> : <p className='wayLessSpace'>Admin Status: False</p> }
-            <br/>
-            <br/>
+            <div>
+                <h3 className='wayLessSpace'>{user.name}{" "}<button onClick={onEdit} className='userEdit'>Edit ✏️</button></h3>
+                <p className='wayLessSpace'>{user.title}</p>
+                <p className='wayLessSpace'>{user.email}</p>
+                {user.admin ? <p className='wayLessSpace'>Admin Status: True</p> : <p className='wayLessSpace'>Admin Status: False</p> }
+                <br/>
+                <br/>
+            </div>
         </div>
     )
 }
