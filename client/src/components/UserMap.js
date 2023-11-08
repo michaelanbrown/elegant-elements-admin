e041c87ac4b86129fade9d6f9c8764e8d9c70031
Michaela Brown <m_brownbrown@yahoo.com>
2023-11-07 16:19:07 -0800

    added ternary operator for edit and elements to show

--- client/src/components/UserMap.js
+++ client/src/components/UserMap.js
@@ -10,14 +10,14 @@ function onEdit() {
 
     return (
         <div>
-            <div>
+            {!edit ? <div>
                 <h3 className='wayLessSpace'>{user.name}{" "}<button onClick={onEdit} className='userEdit'>Edit ✏️</button></h3>
                 <p className='wayLessSpace'>{user.title}</p>
                 <p className='wayLessSpace'>{user.email}</p>
                 {user.admin ? <p className='wayLessSpace'>Admin Status: True</p> : <p className='wayLessSpace'>Admin Status: False</p> }
                 <br/>
                 <br/>
-            </div>
+            </div> : null }
         </div>
     )
 }
