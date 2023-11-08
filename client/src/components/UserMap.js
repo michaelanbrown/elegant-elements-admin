ee54730c18b6e4b12021d2f376d6abcbf05811a0
Michaela Brown <m_brownbrown@yahoo.com>
2023-11-07 16:27:23 -0800

    moved around h3 and an input

--- client/src/components/UserMap.js
+++ client/src/components/UserMap.js
@@ -32,10 +32,9 @@ function handleChange(e) {
             </div> : null }
             {edit ? <div>
                         <form>
-                            <h3 className='wayLessSpace'>{user.name}{" "}<button onClick={onEdit} className='userEdit'>Edit ✏️</button></h3>
-                            <input className="formFields" type='text' name='name' value={formData.name} onChange={handleChange} />
+                            <h3><input className="formFields" type='text' name='name' value={formData.name} onChange={handleChange} /><button onClick={onEdit} className='userEdit'>Edit ✏️</button></h3>
                             <br/>
-                            <input className="formFields" type='text' name='title' value={formData.title} onChange={handleChange} />
+                            Title: <input className="formFields" type='text' name='title' value={formData.title} onChange={handleChange} />
                             <br/>
                             <input className="formFields" type='text' name='admin' value={formData.admin} onChange={handleChange} />
                         </form>
