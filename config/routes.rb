dd1743edaeab72cec972256b37eef4822a6e2c58
michaelanbrown <m_brownbrown@yahoo.com>
2023-11-12 14:40:02 -0800

    added update as a user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :products
   resources :categories
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :create, :update]
 
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
