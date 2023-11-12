c5bfe1291943812fbef4653ce42db415b1918158
michaelanbrown <m_brownbrown@yahoo.com>
2023-11-12 14:42:47 -0800

    changed from user to formdata info

--- client/src/components/UserMap.js
+++ client/src/components/UserMap.js
@@ -62,10 +62,10 @@ function updateUser(e) {
     return (
         <div>
             {!edit ? <div>
-                <h3 className='wayLessSpace'>{user.name}{" "}<button onClick={onEdit} className='userEdit'>Edit ✏️</button></h3>
-                <p className='wayLessSpace'>{user.title}</p>
+                <h3 className='wayLessSpace'>{formData.name}{" "}<button onClick={onEdit} className='userEdit'>Edit ✏️</button></h3>
+                <p className='wayLessSpace'>{formData.title}</p>
                 <p className='wayLessSpace'>{user.email}</p>
-                {user.admin ? <p className='wayLessSpace'>Admin Status: True</p> : <p className='wayLessSpace'>Admin Status: False</p> }
+                {formData.admin ? <p className='wayLessSpace'>Admin Status: True</p> : <p className='wayLessSpace'>Admin Status: False</p> }
                 <br/>
                 <br/>
             </div> : null }
