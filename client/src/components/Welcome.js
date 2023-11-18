76a56b4a1eca923019f7211e7c40fbe4271b8600
michaelanbrown <m_brownbrown@yahoo.com>
2023-11-18 09:05:41 -0800

    adding login to css

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -19,7 +19,7 @@ function Welcome() {
             </div>
             <br/>
             <br/>
-            {currentUser == false ? <p><NavLink className='navbarlink' to="/login">Login</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;</p> : null }
+            {currentUser == false ? <p><NavLink className='login' to="/login">Login</NavLink>&nbsp;&nbsp;&nbsp;&nbsp;</p> : null }
         </div>
     )
 }
