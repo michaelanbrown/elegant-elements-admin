9e8bed51a94c283ab258aefb62dff27363ac6ea1
michaelanbrown <m_brownbrown@yahoo.com>
2023-11-20 18:40:30 -0800

    edited migration file to add default:

--- db/migrate/20231121023801_add_active_to_users.rb
+++ db/migrate/20231121023801_add_active_to_users.rb
@@ -1,5 +1,5 @@
 class AddActiveToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :active, :boolean, true
+    add_column :users, :active, :boolean, default: true
   end
 end
