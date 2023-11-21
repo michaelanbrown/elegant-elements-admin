991beeb86d891cca689cdf609971fb4b94eb8027
michaelanbrown <m_brownbrown@yahoo.com>
2023-11-20 18:49:54 -0800

    remove getUsers as a prop

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -3,7 +3,7 @@ import '../App.css'
 import { UserContext } from './context/User';
 import { useNavigate } from 'react-router-dom';
 
-function Signup({ users, setUsers, getCategories, getProducts, getUsers }) {
+function Signup({ users, setUsers, getCategories, getProducts }) {
     const { currentUser, setCurrentUser } = useContext(UserContext);
     const [errors, setErrors] = useState([])
     const [formData, setFormData] = useState({
