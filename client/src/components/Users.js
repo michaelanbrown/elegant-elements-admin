0c2fed91928400cdbc18500fe4f9368b0fe54d51
michaelanbrown <m_brownbrown@yahoo.com>
2023-11-27 18:15:05 -0800

    improted useEffect

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,5 +1,5 @@
 import '../App.css'
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import UserMap from './UserMap';
 
